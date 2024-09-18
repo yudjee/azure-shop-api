a1523879f1b58d0c4fa505934bf964fc625a03e2
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-09-18 19:45:56 +0300

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.92.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "front_end_rg" {
+  name     = "rg-frontend-sand-ne-001"
+  location = "northeurope"
+}
